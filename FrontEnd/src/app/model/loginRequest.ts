79ea029146931b1d2667764d269ad25dc14bbb45
ZERO-JIFAR <101071380+ZERO-JIFAR@users.noreply.github.com>
2022-10-27 21:26:37 -0300

    Delete FrontEnd directory

--- FrontEnd/src/app/model/loginRequest.ts
+++
@@ -1,9 +0,0 @@
-export class LoginRequest{
-    username:String;
-    password:String;
- 
-    constructor(username:String,password:String){
-     this.username=username;
-     this.password=password;
-    }
- }
--- FrontEnd/src/app/model/signupRequest.ts
+++
@@ -1,6 +0,0 @@
-export class SignupRequest{
-    username!: String;
-    email!:String;
-    password!: String;
-    authorities!: String[];
-}
